db937fc9770a25a6e8dd2834da2a99c74635256e
Njs <99959105+Mrjoseph01@users.noreply.github.com>
2023-11-27 23:41:44 +0200

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const books = [
+  { id: 1000, title: 'things fall apart', author: 'Chinua Achebe', isbn: '1234567890' },
+];
+
+router.get('/books', (req, res) => {
+  res.json(books);
+});
+
+// Add more route handlers based on your tasks
+
+module.exports = router;
 
